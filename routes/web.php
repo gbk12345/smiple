426773cc384b733f993a73ef1209766b0133de39
apple <apple.gmail.com>
2019-05-19 20:11:44 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,17 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
+/*用户curd*/
 Route::resource('users','UsersController');
+
+/**
+ * 用户会话
+ */
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
