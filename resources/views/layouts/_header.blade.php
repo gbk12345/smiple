75864d765c761d154e1508c01f60785a85234a5c
apple <apple.gmail.com>
2019-05-19 20:17:09 +0800

    登录成功后导航逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container ">
-        <a class="navbar-brand" href="{{route('help')}}">Weibo App</a>
+        <a class="navbar-brand" href="{{ route('help') }}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a></li>
-            <li class="nav-item" ><a class="nav-link" href="{{route('login')}}">登录</a></li>
+            @if (Auth::check())
+                <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">编辑资料</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{route('users.show',Auth::user())}}">个人中心</a></li>
+                <li class="nav-item">
+                    <a class="" id="" href="#">
+                        <form action="{{ route('logout') }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                        </form>
+                    </a>
+                </li>
+            @else
+                <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+                <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+            @endif
         </ul>
     </div>
 </nav>
